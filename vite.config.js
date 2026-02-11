dbcd71ee59ecee7662c8cdee032c9930803c12d7
Zach Vanderbosch <zachary.vanderbosch@austin.utexas.edu>
2026-02-11 06:33:45 -0600

    Add API proxy for ngrok exposure

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
       '/uploads': {
         target: 'http://localhost:3001',
         changeOrigin: true
